1af2978b500eb8453b28ab245dc423df2dea5ad6
chakri-chax <lovachakravarthi@gmail.com>
2023-05-31 11:49:49 +0530

    cOMMIT

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { ethers } from 'ethers';
+import React, { useState } from 'react'
+import Web3Modal from 'web3modal';
+
+const providerOptions = {
+  // coinbasewallet:{
+  //   package:CoinbaseWalletSDK,
+  //   options:{
+  //               appName:"Wallet demo",
+  //               infuraId:{3:"https://polygon-mumbai.infura.io/v3/4cf6fddedfd54da5bde77099cbfc6c41"}
+  //           }
+  // }
+  
+  }
 function App() {
+
+  const [web3Provider,setweb3Provider] = useState(null);
+
+  async function connectWallet()
+    {
+      try {
+        let web3Modal = new Web3Modal({cacheProvider:false,providerOptions,});
+  
+        const web3ModalInstance = await web3Modal.connect();
+        const web3ModalProvider = new ethers.providers.Web3Provider(web3ModalInstance);
+        if(web3ModalProvider)
+          {
+            setweb3Provider(web3ModalProvider);
+          }
+        
+      } catch (error) {
+        console.log(error);
+        
+      }
+    }
   return (
+    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <p>Wallet Connect site</p>
+
+      {
+        web3Provider == null 
+        ?(
+          <button onClick={connectWallet}>
+            CONNECT METAMASK
+              </button>
+        ) 
+        :
+        (
+          <><p> connected</p>
+          <p>Address:{web3Provider.provider.selectedAddress}</p></>
+        )
+      }
+
+
       </header>
     </div>
   );
 }
 
 export default App;
+
+
